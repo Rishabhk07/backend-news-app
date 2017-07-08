5da8ec76c428e68e762daa1c1788bbb7aa98a569
Rishabh Khanna <rishabh@ubuntu-512mb-blr1-01.localdomain>
2017-07-08 07:03:24 +0000

    server chnages on DO
    Signed-off-by: Rishabh Khanna <rishabh@ubuntu-512mb-blr1-01.localdomain>

---
+++ app.js
@@ -0,0 +1,26 @@
+const  express = require('express');
+const request = require('request');
+const app = express();
+function requestNewsJson(msid) {
+
+}
+
+var port = process.env.PORT || 9890;
+
+app.use('/' , express.static(__dirname + "/public"));
+
+const routes = {
+    news: require('./routes/news'),
+    sequelize : require('./routes/test'),
+    parse: require('./routes/parse')
+};
+
+app.use('/news' , routes.news);
+
+app.use('/test', routes.sequelize);
+
+app.use('/parse', routes.parse);
+
+app.listen(9890 , ()=> {
+    console.log("magic happens at " + port);
+});
--- cronScripts/cronjob-script.sh
+++ cronScripts/cronjob-script.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-node ./cronScripts/scrapeApi.js
+#!/bin/bash
+node ./scrapeApi.js
