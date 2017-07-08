519ec70ed353105c4fe9927c0c58e4cb311b742e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-07-08 19:21:46 +0530

    counter saved in file after cron
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- cronScripts/scrapeApi.js
+++ cronScripts/scrapeApi.js
@@ -3,13 +3,19 @@
  */
 const newsCategory = require("../model/NewsMsid");
 const requestNews = require("../utils/requestNews");
+const fs = require('fs');
 for(let key in newsCategory){
     let counter = 0;
-    console.log(newsCategory[key]);
+    console.log(newsCategory[key]);git
         requestNews.fetchNews(newsCategory[key],function (body) {
             counter++;
             console.log("counter: " + counter);
+            fs.appendFile("/home/rishabh/newsapp/backend-news-app/cronScrips/ApiFetched",counter+"\n",(err)=>{
+                if(err) throw err;
+                console.log("Data Was appen to File");
+            })
     })
-
 }
+
+
 
