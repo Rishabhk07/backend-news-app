bd00c8ee28ae465ca0020d6764a5e1ba3378cda2
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-26 11:39:11 +0530

    localhost changed to 0.0.0.0
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- cronScripts/requestNotification.js
+++ cronScripts/requestNotification.js
@@ -3,7 +3,7 @@
  */
 const axios = require('axios');
 
-axios.get('http://localhost:9890/notification/notify').then(function (response) {
+axios.get('http://0.0.0.0:9890/notification/notify').then(function (response) {
     console.log(response)
 }).catch(function (err) {
     console.log("err in notification request");
