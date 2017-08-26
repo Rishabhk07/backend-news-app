12587d4eb3a609954f9f80e250d48ec6ba07aac6
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-26 11:43:02 +0530

    server addr change
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- cronScripts/requestNotification.js
+++ cronScripts/requestNotification.js
@@ -3,7 +3,7 @@
  */
 const axios = require('axios');
 
-axios.get('http://139.59.35.58:9890/notification/notify').then(function (response) {
+axios.get('http://139.59.35.58:90/notification/notify').then(function (response) {
     console.log(response)
 }).catch(function (err) {
     console.log("err in notification request");
