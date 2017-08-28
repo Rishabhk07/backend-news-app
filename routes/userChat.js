3b06af305efc529a200f281f6926f1596d0023b2
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-28 23:32:58 +0530

    user chat route to get saved chats
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- routes/userChat.js
+++ routes/userChat.js
@@ -6,27 +6,12 @@ const route = express.Router();
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const Sequelize = require('sequelize');
 const chatTable = require('../models/userChats');
 const bodyParser = require('body-parser');
 const newsModel = require('../models/NewsModel');
+const seq = require('../models/sequelizeConnection');
 
-const sequelize = new Sequelize({
-    host: 'localhost',
-    username: 'rishabh',
-    database: 'newsapp',
-    password: 'beyblade',
-    dialect: 'mysql',
-    dialectOptions: {
-        charset: 'utf8mb4'
-    },
-    pool: {
-        max: 50,
-        min: 0,
-        idle: 10000
-    }
-});
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 function getTableName(msid) {
     switch (msid) {
         case "48986328":
@@ -62,80 +47,9 @@ function getTableName(msid) {
         default:
             return "not matched"
     }
-
 }
 
-io.on('connection', (socket) => {
-    console.log("Socket evoked")
-    setTimeout(sendHearbeat,2500);
-    socket.on('pong',function (msg) {
-        console.log(msg)
-    })
-
-    function sendHearbeat() {
-        setTimeout(sendHearbeat, 2500);
-        io.sockets.emit('ping', { beat : 1 });
-    }
-
-    socket.on('join_room',function (msg) {
-        let chatRoom  = JSON.parse(msg);
-        socket.join(chatRoom.news_id);
-        console.log("user joined room " + chatRoom.news_id )
-        console.log("socket id : " + socket.id);
-    });
-
-    socket.on('leave_group',function (msg) {
-        let chatRoom = JSON.parse(msg)
-        socket.leave(chatRoom.news_id,function () {
-            console.log("banda has left the group ")
-
-        });
-        console.log("user leaved the group " + chatRoom.news_id)
-        console.log("socket id " + socket.id);
-    })
-
-    socket.on('new_message', function (msg) {
-        console.log("socket id : " + socket.id)
-        let json = JSON.parse(msg);
-        console.log(msg);
-        // socket.join(json.news_id);
-        let news_table = getTableName(json.msid)
-
-        console.log(getTableName(json.msid))
-        console.log(msg)
-        let Chats = chatTable(news_table);
-        Chats.build({
-            message: json.message,
-            news_type:news_table,
-            msid: json.msid,
-            news_id: json.news_id,
-            from: json.from,
-            anonym: json.anonym
-        }).save().then(function (response) {
-            console.log("saved the user chat successfully")
-            console.log(response)
-            console.log("Socket id to return : " + socket.id);
-            console.log(io.sockets.adapter.rooms[json.news_id]);
-            io.sockets.in(json.news_id).emit('from_server',response )
-            let NewsTable = newsModel(news_table, sequelize);
-            NewsTable.findOne({
-                where:{id: json.news_id}
-            }).then(function (news) {
-                news.increment('chats',{by: 1})
-            }).catch(function (err) {
-                console.log(err)
-                console.log("cannot increment chat message");
-            })
-
-        })
-    });
-
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    })
-});
-
-app.post('/getChats',(req,res)=>{
+route.post('/getChats',(req,res)=>{
     console.log("request at getChats newsId: ");
     console.log(req.body.news_id)
     console.log("msid: ")
@@ -158,8 +72,8 @@ app.post('/getChats',(req,res)=>{
 });
 
 
-module.exports = route;
+module.exports = {route,getTableName};
 
-http.listen('9999', () => {
-    console.log("Socket server started at 9999")
-});
+// http.listen('9999', () => {
+//     console.log("Socket server started at 9999")
+// });
