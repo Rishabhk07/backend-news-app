b2a7cb00f764560072eaf9bdb307e78770552c2e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-30 13:20:04 +0530

    some refactoring
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- models/models.js
+++ models/models.js
@@ -1,28 +1,5 @@
-// /**
-//  * Created by rishabhkhanna on 28/08/17.
-//  */
-// const sequelize = require('sequelize');
-// const newsModel = require('NewsModel');
-// const userChats = require('userChats');
-// const userModel = require('userModel');
-// const userNews = require('userNews');
-// const db = new sequelize({
-//     host: 'localhost',
-//     username: 'rishabh',
-//     database: 'newsapp',
-//     password: 'beyblade',
-//     dialect: 'mysql',
-//     dialectOptions: {
-//         charset: 'utf8mb4'
-//     },
-//     pool: {
-//         max: 50,
-//         min: 0,
-//         idle: 10000
-//     }
-// });
-console.log("RISHABH KHANNA")
-//NewsModel
+/*created by Rishabh Khanna*/
+const seq = require('./sequelizeConnection');
 
 
 
