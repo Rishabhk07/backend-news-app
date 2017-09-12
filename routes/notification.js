0f71cdc3f00a6b75add09d957698a59d4943f454
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-09-12 11:39:00 +0530

    unique user notification pnly
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- routes/notification.js
+++ routes/notification.js
@@ -13,64 +13,29 @@ let serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 },"second");
-
+let currentBreifs = null;
+let userFcmToken = [];
+let counter  = 0;
 route.get('/notify', (req, res) => {
+    currentBreifs = null;
+    userFcmToken = [];
+    counter  = 0;
 console.log("request to notificatoin log")
     User.findAll().then(function (response) {
-        console.log("user found");
-        console.log(response);
-        for (let user of response) {
-            if (user.notification === true) {
-                //sending notification fo rbrief only
-                console.log("ntification : user found ");
-                let thisNews = getNewsTable("briefs", seq.db);
-                thisNews.findOne({
-                    order: [['createdAt', 'DESC']]
-                }).then(function (response) {
-                    let d = new Date();
-                    console.log(moment().format());
+        for (let key in response) {
+            if (response[key].notification === true) {
+                //sending notification for briefs only
+                console.log("inside IF")
+                if(userFcmToken.indexOf(response[key].fcm_token) === -1){
+                    userFcmToken.push(response[key].fcm_token)
+                }
 
-                    console.log(new Date(response.createdAt));
-                    console.log(new Date(response.createdAt) - Date.now());
-                    let startDate = moment(response.createdAt, 'YYYY-M-DD HH:mm:ss')
-                    let endDate = moment(new Date(Date.now()), 'YYYY-M-DD HH:mm:ss')
-                    let timeElapsed = moment(endDate).diff(startDate, 'hours');
-                    console.log(moment(endDate).diff(startDate, 'hours'));
-                    if (timeElapsed < 50) {
-
-                        console.log(startDate)
-                        console.log(endDate)
-
-                        console.log(response.hl)
-                        let payload = {
-                            data: {
-                                news_id: "briefs",
-                                title: response.hl,
-                                image: response.imageid,
-                                detail: response.syn
-                            }
-                        };
-                        console.log("TOKEN")
-                        console.log(user.fcm_token);
-                        admin.messaging().sendToDevice(user.fcm_token, payload)
-                            .then(function (response) {
-                                console.log("successfully send message on brief");
-                                console.log(response.results)
-                                res.send({success: true})
-                            }).catch(function (err) {
-                            console.log("Error in sending message " + err);
-                            res.send({success: false})
-                        })
-                    }
-                }).catch(function (err) {
-                    console.log(err)
-                })
                 //end
             } else {
                 // getting to all the topic selected by user and sending  notification to that only
                 console.log("user in topic selected");
                 let anyTure = true;
-                for (let thisTopic of user.topics) {
+                for (let thisTopic of response[key].topics) {
                     if (thisTopic.value === true) {
                         console.log("itertaing topic")
                         anyTure = false;
@@ -92,7 +57,7 @@ console.log("request to notificatoin log")
                                 }
                             };
 
-                            admin.messaging().sendToDevice(user.fcm_token, payload)
+                            admin.messaging().sendToDevice(response[key].fcm_token, payload)
                                 .then(function (response) {
                                     console.log("Successfully send message");
                                     console.log(response)
@@ -135,8 +100,8 @@ console.log("request to notificatoin log")
                                 }
                             };
                             console.log("TOKEN")
-                            console.log(user.fcm_token);
-                            admin.messaging().sendToDevice(user.fcm_token, payload)
+                            console.log(response[key].fcm_token);
+                            admin.messaging().sendToDevice(response[key].fcm_token, payload)
                                 .then(function (response) {
                                     console.log("successfully send message on brief");
                                     console.log(response.results)
@@ -158,5 +123,56 @@ console.log("request to notificatoin log")
     })
 
 });
+
+function sendToDevices() {
+    console.log("USERS: ")
+    console.log(userFcmToken);
+    admin.messaging().sendToDevice(userFcmToken, currentBreifs)
+        .then(function (response) {
+            console.log("successfully send message on brief");
+            console.log(response.results)
+        }).catch(function (err) {
+        console.log("Error in sending message " + err);
+    })
+}
+
+function getCurrentBriefs(){
+    let thisNews = getNewsTable("briefs", seq.db);
+    thisNews.findOne({
+        order: [['createdAt', 'DESC']]
+    }).then(function (response) {
+        let d = new Date();
+        console.log(moment().format());
+
+        console.log(new Date(response.createdAt));
+        console.log(new Date(response.createdAt) - Date.now());
+        let startDate = moment(response.createdAt, 'YYYY-M-DD HH:mm:ss')
+        let endDate = moment(new Date(Date.now()), 'YYYY-M-DD HH:mm:ss')
+        let timeElapsed = moment(endDate).diff(startDate, 'hours');
+        console.log(moment(endDate).diff(startDate, 'hours'));
+        // if (timeElapsed < 2) {
+
+        console.log(startDate)
+        console.log(endDate)
+
+        console.log(response.hl)
+        currentBreifs = {
+            data: {
+                news_id: "briefs",
+                title: response.hl,
+                image: response.imageid,
+                detail: response.syn,
+            }
+        };
+        sendToDevices();
+        console.log("TOKEN")
+
+        // }
+    }).catch(function (err) {
+        console.log(err);
+        console.log("thisNews find one")
+    })
+
+
 
 module.exports = route;
