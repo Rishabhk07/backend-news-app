24abaeea5829907ee13b6fa908eb56ebeaf8d93d
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-16 16:37:04 +0530

    model for fcm keys table
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- models/GcmKeyModel.js
+++ models/GcmKeyModel.js
@@ -1,3 +1,21 @@
 /**
  * Created by rishabhkhanna on 16/10/17.
  */
+const sequelize = require('./sequelizeConnection');
+let fcmSchema = {};
+(function defineGcmModel() {
+    fcmSchema = sequelize.db.define('fcmKey',{
+        fcm_token: {
+            type: sequelize.Sequelize.DataTypes.STRING,
+            unique: true,
+        },
+        id: {
+            type: sequelize.Sequelize.DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    });
+    fcmSchema.sync();
+})();
+
+module.exports = fcmSchema;
